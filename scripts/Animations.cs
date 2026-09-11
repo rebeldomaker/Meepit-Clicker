368c1d3f0681322e06f4ce108f833909ce17e0b8
Reb <rebeldomaker@icloud.com>
2026-09-11 19:47:39 +0200

    //

--- scripts/Animations.cs
+++ scripts/Animations.cs
@@ -1,16 +1,26 @@
 using Godot;
-using System;
-/*
+
 public partial class Animations : MarginContainer
 {
-	// Called when the node enters the scene tree for the first time.
+	private TextureButton _clickButton;
+
 	public override void _Ready()
 	{
+		_clickButton = GetNode<TextureButton>("CenterContainer/ClickButton");
+		
+		// Sets the pivot point to the exact center of the button for scaling animations
+		_clickButton.PivotOffset = _clickButton.Size / 2;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void _on_click_button_button_down()
 	{
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(_clickButton, "scale", new Vector2(0.9f, 0.9f), 0.1);
+	}
+
+	private void _on_click_button_button_up()
+	{
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(_clickButton, "scale", new Vector2(1.0f, 1.0f), 0.1);
 	}
 }
-*/
